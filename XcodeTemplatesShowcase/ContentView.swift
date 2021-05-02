ae10a03e1d80a45b7bb59a6e2ee2089798b7a900
Damian Włodarczyk <damian.wlodarczyk.dod@gmail.com>
2021-05-02 15:41:59 +0200

    Initial Commit

---
+++ XcodeTemplatesShowcase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodeTemplatesShowcase
+//
+//  Created by Damian Włodarczyk on 02/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodeTemplatesShowcase/XcodeTemplatesShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeTemplatesShowcaseApp.swift
+//  XcodeTemplatesShowcase
+//
+//  Created by Damian Włodarczyk on 02/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct XcodeTemplatesShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
